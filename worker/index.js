c015082012f04d3cb77de90e475f4eb3ea143a40
dacapo9006 <dacapo9006@gmail.com>
2026-08-19 23:22:37 +0900

    chore: 슬로 헤리티지 소스 전체 동기화 (오버투어리즘 배너, 태그, 코스 편집 등 최신 기능 반영)

---
+++ worker/index.js
@@ -0,0 +1,55 @@
+/**
+ * 슬로 헤리티지 - TourAPI 프록시 Worker
+ * Cloudflare Workers에서 실행
+ * - API 키를 환경변수(TOUR_API_KEY)로 보호
+ * - CORS 처리
+ * - 실시간 API 호출
+ */
+
+const TOUR_API_BASE = 'https://apis.data.go.kr/B551011/KorService2';
+
+const ALLOWED_OPS = [
+  'searchKeyword2', 'areaBasedList2', 'locationBasedList2',
+  'detailCommon2', 'detailIntro2', 'detailInfo2',
+  'searchFestival2', 'detailImage2',
+];
+
+const CORS = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type',
+};
+
+export default {
+  async fetch(request, env) {
+    if (request.method === 'OPTIONS') {
+      return new Response(null, { headers: CORS });
+    }
+
+    const url = new URL(request.url);
+    const op = url.pathname.replace('/api/tour/', '');
+
+    if (!ALLOWED_OPS.includes(op)) {
+      return Response.json({ error: '허용되지 않은 오퍼레이션' }, { status: 400, headers: CORS });
+    }
+
+    const params = new URLSearchParams(url.search);
+    params.set('serviceKey', env.TOUR_API_KEY);
+    if (!params.has('_type')) params.set('_type', 'json');
+    if (!params.has('MobileOS')) params.set('MobileOS', 'ETC');
+    if (!params.has('MobileApp')) params.set('MobileApp', 'SlowHeritage');
+
+    try {
+      const res = await fetch(`${TOUR_API_BASE}/${op}?${params}`, {
+        cf: { cacheTtl: 0, cacheEverything: false },
+      });
+      const data = await res.text();
+      return new Response(data, {
+        status: res.status,
+        headers: { 'Content-Type': 'application/json; charset=utf-8', ...CORS },
+      });
+    } catch (err) {
+      return Response.json({ error: 'TourAPI 호출 실패', detail: err.message }, { status: 502, headers: CORS });
+    }
+  },
+};
